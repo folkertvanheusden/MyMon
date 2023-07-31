339dbe2970c5846ac188af88c48d2f74c4b2b977
Folkert van Heusden <folkert@vanheusden.com>
2023-07-31 15:21:02 +0200

    db schema update

--- schema.sql
+++ schema.sql
@@ -5,8 +5,7 @@ CREATE TABLE `check_local` (
   PRIMARY KEY (`nr`)
 );
 
-INSERT INTO `check_local` VALUES (1,'/usr/lib/nagios/plugins/check_ping -H %host% -4 -p 5 -c 500,50% -w 400,20%','ping');
-INSERT INTO `check_local` VALUES (2,'/usr/lib/nagios/plugins/check_http -H %host% -E -j HEAD','HTTP');
+INSERT INTO `check_local` VALUES (1,'/usr/lib/nagios/plugins/check_ping -H %host% -4 -p 5 -c 500,50% -w 400,20%','ping'),(2,'/usr/lib/nagios/plugins/check_http -H %host% -E -j HEAD','HTTP'),(3,'/usr/lib/nagios/plugins/check_tcp -H %host% -p %port% -4 -w 2.0 -c 5.0','IRC');
 
 CREATE TABLE `check_remote` (
   `nr` int(6) NOT NULL AUTO_INCREMENT,
@@ -26,11 +25,31 @@ CREATE TABLE `checks` (
   `status` enum('ok','warning','fatal','unknown') NOT NULL,
   `host_nr` int(6) NOT NULL,
   `last_check_result_str` text NOT NULL,
+  `contact_nr` int(6) NOT NULL,
+  PRIMARY KEY (`nr`),
+  KEY `contact_nr` (`contact_nr`),
+  KEY `host_nr` (`host_nr`),
+  CONSTRAINT `checks_ibfk_1` FOREIGN KEY (`contact_nr`) REFERENCES `contacts` (`nr`),
+  CONSTRAINT `checks_ibfk_2` FOREIGN KEY (`host_nr`) REFERENCES `hosts` (`nr`)
+);
+
+CREATE TABLE `contacts` (
+  `nr` int(6) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
   PRIMARY KEY (`nr`)
 );
 
 CREATE TABLE `hosts` (
   `nr` int(6) NOT NULL AUTO_INCREMENT,
   `host` varchar(255) NOT NULL,
+  PRIMARY KEY (`nr`)
+);
+
+CREATE TABLE `keyvalue` (
+  `nr` int(6) NOT NULL AUTO_INCREMENT,
+  `host_nr` int(6) NOT NULL,
+  `check_nr` int(6) NOT NULL,
+  `key` varchar(255) NOT NULL,
+  `value` text NOT NULL,
   PRIMARY KEY (`nr`)
 );
